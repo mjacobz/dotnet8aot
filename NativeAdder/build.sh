ff82dc910539e63ec4af882036fbf41c7f1ced6e
mjacobz <146822320+mjacobz@users.noreply.github.com>
2024-06-04 12:27:37 +0200

    Update build.sh

--- NativeAdder/build.sh
+++ NativeAdder/build.sh
@@ -6,5 +6,5 @@ docker rm nativeadder_container
 echo "Building the cpp file"
 aarch64-linux-gnu-g++ -o ./main main.cpp
 
-echo "Copying everything to qemu"
+echo "Copying net8 aot lib to qemu"
 scp NativeAdder.so root@192.168.7.2:/usr/share/lib-test/

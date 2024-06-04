53ec4df4cd1fd5949a0d58f3db193ce28f1cc7bc
mjacobz <146822320+mjacobz@users.noreply.github.com>
2024-06-04 12:16:16 +0200

    Add files via upload

---
+++ NativeAdder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+
+namespace NativeAdder;
+
+public class Program
+{
+    private static System.Timers.Timer _timer = new (1000);
+    private static Thread _thread;
+    private static bool _isRunning;
+
+    [UnmanagedCallersOnly(EntryPoint = "Add")]
+    public static int Add(int a, int b)
+    {
+        _timer.AutoReset = true;
+        _timer.Enabled = true;
+        _timer.Elapsed += (x, y) => { Console.WriteLine("delegate tiggered"); };
+        System.Threading.Thread.Sleep(10000);
+        //_timer.Enabled = false;
+
+        _thread = new Thread(new ThreadStart(KeepThreadAlive));
+        _isRunning = true;
+        _thread.Start();
+
+        return a + b;
+    }
+
+     static void KeepThreadAlive()
+    {
+        while (true)
+        {
+            Console.WriteLine("Thread is still running...");
+            Thread.Sleep(1000);  // Sleep for 1 second
+        }
+    }
+}

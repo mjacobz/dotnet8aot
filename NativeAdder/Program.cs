c12606318f1b9aec08e81381263312e684fd4b71
mjacobz <146822320+mjacobz@users.noreply.github.com>
2024-06-04 12:26:58 +0200

    Update Program.cs

--- NativeAdder/Program.cs
+++ NativeAdder/Program.cs
@@ -5,8 +5,6 @@ namespace NativeAdder;
 public class Program
 {
     private static System.Timers.Timer _timer = new (1000);
-    private static Thread _thread;
-    private static bool _isRunning;
 
     [UnmanagedCallersOnly(EntryPoint = "Add")]
     public static int Add(int a, int b)
@@ -16,20 +14,7 @@ public class Program
         _timer.Elapsed += (x, y) => { Console.WriteLine("delegate tiggered"); };
         System.Threading.Thread.Sleep(10000);
         //_timer.Enabled = false;
-
-        _thread = new Thread(new ThreadStart(KeepThreadAlive));
-        _isRunning = true;
-        _thread.Start();
-
+        
         return a + b;
-    }
-
-     static void KeepThreadAlive()
-    {
-        while (true)
-        {
-            Console.WriteLine("Thread is still running...");
-            Thread.Sleep(1000);  // Sleep for 1 second
-        }
     }
 }
